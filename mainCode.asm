443a3f622367b2a7ff826a242febfff6d794c378
Mahela Pandukabhaya <55336039+Mahela-Pandukabhaya@users.noreply.github.com>
2021-07-28 19:40:28 +0530

    Update mainCode.asm
    Initial commenting: header

--- mainCode.asm
+++ mainCode.asm
@@ -1,1 +1,10 @@
-
+;
+; EE322 Embedded Systems Design
+; Year 3 Semester 1
+; Group Project: Coin Sorter and Calculator
+; 
+; Group G1
+;    E/17/146: Jayawickrama, J.P.D.D.M.
+;    E/17/234: Pandukabhaya, V.K.M.
+;    E/17/371: Warnakulasuriya. R.
+; 
