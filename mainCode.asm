29780342040ee0a0e7f0e5af07c89f8c1a889f65
Blah029 <81973454+Blah029@users.noreply.github.com>
2021-07-30 20:54:57 +0530

    Inital branch (#4)
    - Add pseudoCode.txt
    - Add simulation.pdsprj
    - Add state diagram
    - Add sequence diagram
    
    Co-authored-by: Mahela Pandukabhaya <55336039+Mahela-Pandukabhaya@users.noreply.github.com>

--- mainCode.asm
+++ mainCode.asm
@@ -1,10 +1,45 @@
-;
+<<<<<<< Updated upstream
+=======
+;-------------------------------------------------------------------------------
+>>>>>>> Stashed changes
 ; EE322 Embedded Systems Design
 ; Year 3 Semester 1
-; Group Project: Coin Sorter and Calculator
+; Group Project
+; Title: Coin Sorter and Calculator
 ; 
 ; Group G1
+<<<<<<< Updated upstream
+;     E/17/146: Jayawickrama J.P.D.D.M
+;     E/17/234: Pandukabhaya V.K.M
+;     E/17/371: Warnakulasuriya R
+
+=======
 ;    E/17/146: Jayawickrama, J.P.D.D.M.
 ;    E/17/234: Pandukabhaya, V.K.M.
-;    E/17/371: Warnakulasuriya. R.
-; 
+;    E/17/371: Warnakulasuriya, R.
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Header
+#include "p16f84a.inc"      ; Include file
+        list    p = 16f84a  ; Microcontroller model
+        org     0x000       ; Origin
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Constants
+
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Routine "main"
+main
+        bsf     STATUS,5
+        movlw   b'00000000'
+        movwf   TRISB
+        bcf     STATUS, 5
+
+        goto    $
+        end
+;-------------------------------------------------------------------------------
+>>>>>>> Stashed changes
