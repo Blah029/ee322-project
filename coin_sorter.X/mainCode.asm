548de8a05252590f466977e9477178c5e6529b17
Mahela-Pandukabhaya <55336039+Mahela-Pandukabhaya@users.noreply.github.com>
2021-08-06 12:11:40 +0530

    Updated mainCode.asm
    Added support for interrupts.

--- coin_sorter.X/mainCode.asm
+++ coin_sorter.X/mainCode.asm
@@ -12,9 +12,8 @@
 
 ;-------------------------------------------------------------------------------
 ; Header
-#include "p16f84a.inc"      ; Include file
-        list    p = 16f84a  ; Microcontroller model
-        org     0x000       ; Origin
+#include "p16f84a.inc"                  ; Include file
+        list        p = 16f84a          ; Microcontroller model
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
@@ -23,16 +22,62 @@
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
-; Routine "main"
-main
-        bsf     STATUS,5    ; Select Bank 1
-        movlw   b'00000000' ; Move literal to W
-        movwf   TRISB       ; W -> TRISB (port B as output)
-        bcf     STATUS, 5   ; Select Bank 0
+; Define origin and interrupt vectors
+        org         0x000               ; Origin
+        goto        INIT
         
-        movlw   b'11111111' ; -> to W
-        movwf   PORTB       ; W -> PORTB (port B as high)
+        org         0x04                ; Interrupt vector
+        goto        COIN_INSERT
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Routine "INIT"
+;       Executed once
+
+INIT:
+; Set Interrupt Settings
+        bsf         INTCON, INTE        ; Enable the RB0/INT interrupt
         
+        bsf         INTCON, GIE         ; Enable all un-masked (global)
+                                        ; interrupts
+                                    
+        bcf         INTCON, INTF        ; Clear the RB0/INT flag
+        
+        bsf         OPTION_REG, INTEDG  ; Interrupt on rising edge of
+                                        ; RB0/INT pin
+
+; Set PORTA as output, and PORTB as input (all pins)
+        bsf         STATUS, 5           ; Bank 1 select
+        movlw       b'11111111'
+        movwf       TRISB
+        movlw       b'00000'
+        movwf       TRISA
+        bcf         STATUS, 5           ; Bank 0 select
+
+; Initialize PORTA and PORTB
+        movlw       b'00000000'
+        movwf       PORTB
+        movlw       b'00000'
+        movwf       PORTA
+        
+;-------------------------------------------------------------------------------
+        
+;-------------------------------------------------------------------------------
+; Routine "MAIN"
+MAIN:
+        ; Do something here
+        
+        goto        MAIN                ; Go to the MAIN routine again (loop)
+
+;-------------------------------------------------------------------------------
+; ISR "COIN_INSERT"
+COIN_INSERT:
+        ; Do something when a coin is inserted
+        retfie                          ; Return from interrupt
+
+;-------------------------------------------------------------------------------
+        
+;-------------------------------------------------------------------------------
         goto    $
         end
 ;-------------------------------------------------------------------------------
