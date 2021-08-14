b0febc155c70deea8234300b0034d1bfded3b224
Mahela-Pandukabhaya <55336039+Mahela-Pandukabhaya@users.noreply.github.com>
2021-08-14 20:48:01 +0530

    Integrated motor control logic to main project
    Motor control code segment transferred to main project. Proteus file updated.

--- coin_sorter.X/mainCode.asm
+++ coin_sorter.X/mainCode.asm
@@ -12,8 +12,21 @@
 
 ;-------------------------------------------------------------------------------
 ; Header
-#include "p16f84a.inc"                  ; Include file
+#include <p16f84a.inc>                  ; Include file for register names
         list        p = 16f84a          ; Microcontroller model
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Pin Designations
+
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Variables
+; Counter variables for delays
+count1  equ         0x0c		; used in a decfsz
+count2  equ         0x0d		; used in a decfsz
+count3  equ         0x0e		; used in a decfsz
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
@@ -27,14 +40,16 @@
         goto        INIT
         
         org         0x04                ; Interrupt vector
-        goto        COIN_INSERT
+        goto        ISR                 ; Unimplemented
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
 ; Routine "INIT"
-;       Executed once
+;       Executed once. Configures settings in PIC.
 
 INIT:
+        bsf         STATUS, RP0         ; Bank 1 select (bit 5)
+
 ; Set Interrupt Settings
         bsf         INTCON, INTE        ; Enable the RB0/INT interrupt
         
@@ -46,12 +61,13 @@ INIT:
         bsf         OPTION_REG, INTEDG  ; Interrupt on rising edge of
                                         ; RB0/INT pin
 
-; Set PORTA as output, and PORTB as input (all pins)
-        bsf         STATUS, 5           ; Bank 1 select
-        movlw       b'11111111'
-        movwf       TRISB
-        movlw       b'00000'
-        movwf       TRISA
+; PORTA, PORTB pin modes
+        movlw       b'11111111'         ; PORTB I/O pattern
+        movwf       TRISB               ; Set PORTB pin modes
+        
+        movlw       b'00000'            ; PORTA I/O pattern
+        movwf       TRISA               ; Set PORTA pin modes
+        
         bcf         STATUS, 5           ; Bank 0 select
 
 ; Initialize PORTA and PORTB
@@ -61,23 +77,215 @@ INIT:
         movwf       PORTA
         
 ;-------------------------------------------------------------------------------
-        
+
 ;-------------------------------------------------------------------------------
 ; Routine "MAIN"
 MAIN:
-        ; Do something here
+        ; Example usage of motor control
+        
+        ;Tip the platform +30
+        movlw       b'00000100'         ; turn on RA2
+	movwf       PORTA               ; 
+        call        MOTOR_PLUS30_ON     ;
+        
+        movlw       b'00000000'         ; turn off RA2
+        movwf       PORTA               ; 
+        call        MOTOR_PLUS30_OFF    ;
+        
+        call        DELAY_1S
+        
+        ;Make the platform horizontal
+        movlw       b'00000100'         ; turn on RA2
+        movwf       PORTA               ; 
+        call        MOTOR_0_ON          ;
+        
+        movlw       b'00000000'         ; turn off RA2
+        movwf       PORTA               ; 
+        call        MOTOR_0_OFF         ;
+        
+        call        DELAY_1S
+        
+        ;Tip the platform -30
+        movlw       b'00000100'         ; turn on RA2
+	movwf       PORTA               ; 
+        call        MOTOR_MINUS30_ON    ;
+        
+        movlw       b'00000000'         ; turn off RA2
+        movwf       PORTA               ; 
+        call        MOTOR_MINUS30_OFF   ;
+        
+        call        DELAY_1S
+        
+        ;Make the platform horizontal
+        movlw       b'00000100'         ; turn on RA2
+        movwf       PORTA               ; 
+        call        MOTOR_0_ON          ;
+        
+        movlw       b'00000000'         ; turn off RA2
+        movwf       PORTA               ; 
+        call        MOTOR_0_OFF         ;
+        
+        call        DELAY_1S
         
         goto        MAIN                ; Go to the MAIN routine again (loop)
 
 ;-------------------------------------------------------------------------------
-; ISR "COIN_INSERT"
-COIN_INSERT:
-        ; Do something when a coin is inserted
+; Subroutine "READ_SENSOR_VALUE"
+READ_SENSOR_VALUE:
+;        unsigned long Count;
+;        unsigned char i;
+;        pinMode(DT, OUTPUT);
+;        digitalWrite(DT,HIGH);
+;        digitalWrite(SCK,LOW);
+;        Count=0
+;        pinMode(DT, INPUT);
+;        while(digitalRead(DT));
+;        for (i=0;i<24;i++)
+;        {
+;          digitalWrite(SCK,HIGH);
+;          Count=Count<<1;
+;          digitalWrite(SCK,LOW);
+;          if(digitalRead(DT)) 
+;          Count++;
+;        }
+;        digitalWrite(SCK,HIGH);
+;        Count=Count^0x800000;
+;        digitalWrite(SCK,LOW);
+;        return(Count);
+    
+        return
+        
+;-------------------------------------------------------------------------------
+; ISR ""
+ISR:
+        ; Interrupt occurred
         retfie                          ; Return from interrupt
-
 ;-------------------------------------------------------------------------------
         
 ;-------------------------------------------------------------------------------
+; Delay subroutine (general)
+; T = 18 + (count1-1)*3 + (count2-1)*770 + (count3-1)*197140 ns
+DELAY
+	decfsz      count1
+	goto        DELAY
+	
+	decfsz      count2
+	goto        DELAY
+	
+	decfsz      count3
+	goto        DELAY
+	
+  	return
+
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Motor Controller subroutines
+
+MOTOR_PLUS90_ON:
+        movlw       d'149'              ; set layer 1 delay counter
+	movwf       count1              ; 
+	movlw       d'3'                ; set layer 2 delay counter
+	movwf       count2              ; 
+	movlw       d'1'                ; set layer 3 delay counter
+	movwf       count3              ; 
+	call        DELAY               ; 90deg on time
+        
+        return
+        
+MOTOR_PLUS90_OFF:
+	movlw       d'1'                ; set layer 1 delay counter
+	movwf       count1              ; 
+	movlw       d'1'                ; set layer 2 delay counter
+	movwf       count2              ; 
+	movlw       d'3'                ; set layer 3 delay counter
+	movwf       count3              ; 
+	call        DELAY               ; 90deg off time
+        
+        return
+
+MOTOR_0_ON:
+        movlw       d'238'              ; set layer 1 delay counter
+	movwf       count1              ; 
+	movlw       d'2'                ; set layer 2 delay counter
+	movwf       count2              ; 
+	movlw       d'1'                ; set layer 3 delay counter
+	movwf       count3              ; 
+	call        DELAY               ; 0deg on time
+        
+        return
+        
+MOTOR_0_OFF:
+	movlw       d'161'              ; set layer 1 delay counter
+	movwf       count1              ; 
+	movlw       d'1'                ; set layer 2 delay counter
+	movwf       count2              ; 
+	movlw       d'1'                ; set layer 3 delay counter
+	movwf       count3              ; 
+	call        DELAY               ; 0deg off time
+        
+        return
+        
+MOTOR_MINUS30_ON:
+        movlw       d'183'              ; set layer 1 delay counter
+	movwf       count1              ; 
+	movlw       d'2'                ; set layer 2 delay counter
+	movwf       count2              ; 
+	movlw       d'1'                ; set layer 3 delay counter
+	movwf       count3              ; 
+	call        DELAY               ; -30deg on time
+        
+        return
+        
+MOTOR_MINUS30_OFF:
+	movlw       d'217'              ; set layer 1 delay counter
+	movwf       count1              ; 
+	movlw       d'1'                ; set layer 2 delay counter
+	movwf       count2              ; 
+	movlw       d'1'                ; set layer 3 delay counter
+	movwf       count3              ; 
+	call        DELAY               ; -30deg off time
+        
+        return
+
+MOTOR_PLUS30_ON:
+        movlw       d'37'               ; set layer 1 delay counter
+	movwf       count1              ; 
+	movlw       d'3'                ; set layer 2 delay counter
+	movwf       count2              ; 
+	movlw       d'1'                ; set layer 3 delay counter
+	movwf       count3              ; 
+	call        DELAY               ; +30deg on time
+        
+        return
+        
+MOTOR_PLUS30_OFF:
+	movlw       d'106'              ; set layer 1 delay counter
+	movwf       count1              ; 
+	movlw       d'1'                ; set layer 2 delay counter
+	movwf       count2              ; 
+	movlw       d'1'                ; set layer 3 delay counter
+	movwf       count3              ; 
+	call        DELAY               ; +30deg off time
+        
+        return
+        
+DELAY_1S:
+	movlw       d'142'              ; set layer 1 delay counter
+	movwf       count1              ; 
+	movlw       d'19'               ; set layer 2 delay counter
+	movwf       count2              ; 
+	movlw       d'6'                ; set layer 3 delay counter
+	movwf       count3              ; 
+	call        DELAY               ; +30deg off time
+        
+        return
+        
+;-------------------------------------------------------------------------------
+INF_LOOP:
         goto    $
+;-------------------------------------------------------------------------------
+        
+;-------------------------------------------------------------------------------
         end
 ;-------------------------------------------------------------------------------
